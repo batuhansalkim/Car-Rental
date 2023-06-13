f460ba481d0aa9492fe9b6a827a4002ef02c8025
batuhan <batusalkim11@gmail.com>
2023-06-14 02:12:34 +0300

    update

--- src/components/UI/FindCarForm.js
+++ src/components/UI/FindCarForm.js
@@ -1,9 +1,45 @@
-import React from 'react'
+import React from "react";
+import "../../styles/find-car-form.css";
+import "../../styles/find-car-form.css";
+import { Form, FormGroup } from "reactstrap";
 
-function FindCarForm() {
+const FindCarForm = () => {
   return (
-    <div>FindCarForm</div>
-  )
-}
+    <Form className="form">
+      <div className=" d-flex align-items-center justify-content-between flex-wrap">
+        <FormGroup className="form__group">
+          <input type="text" placeholder="From address" required />
+        </FormGroup>
 
-export default FindCarForm
+        <FormGroup className="form__group">
+          <input type="text" placeholder="To address" required />
+        </FormGroup>
+
+        <FormGroup className="form__group">
+          <input type="date" placeholder="Journey date" required />
+        </FormGroup>
+
+        <FormGroup className="form__group">
+          <input
+            className="journey__time"
+            type="time"
+            placeholder="Journey time"
+            required
+          />
+        </FormGroup>
+        <FormGroup className="select__group">
+          <select>
+            <option value="ac">AC Car</option>
+            <option value="non-ac">Non AC Car</option>
+          </select>
+        </FormGroup>
+
+        <FormGroup className="form__group">
+          <button className="btn find__car-btn">Find Car</button>
+        </FormGroup>
+      </div>
+    </Form>
+  );
+};
+
+export default FindCarForm;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,30 @@
 import React from 'react'
 import HeroSlider from '../components/UI/HeroSlider'
-import Helmet from '../components/Helmet/Helmet'
+import Helmet from '../components/Helmet/Helmet';
+import FindCarForm from '../components/UI/FindCarForm';
 import "../styles/hero-slider.css";
+import { Container,Row,Col } from 'reactstrap';
 
 function Home() {
   return <Helmet title="Home">
     <section className='p-0 hero__slider-section'>
       <HeroSlider/>
+      
+      <div className="hero__form">
+          <Container>
+            <Row className="form__row">
+              <Col lg="4" md="4">
+                <div className="find__cars-left">
+                  <h2>Find your best car here</h2>
+                </div>
+              </Col>
+
+              <Col lg="8" md="8" sm="12">
+                <FindCarForm />
+              </Col>
+            </Row>
+          </Container>
+        </div>
     </section>
   </Helmet>
 }
