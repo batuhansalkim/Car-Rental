8cfd48519e516f500f75b3bf614ff5dffe186df2
batuhan <batusalkim11@gmail.com>
2023-06-14 00:46:16 +0300

    update

---
+++ src/components/UI/FindCarForm.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function FindCarForm() {
+  return (
+    <div>FindCarForm</div>
+  )
+}
+
+export default FindCarForm
--- src/components/UI/HeroSlider.js
+++ src/components/UI/HeroSlider.js
@@ -31,8 +31,8 @@ const HeroSlider = () => {
       <div className="slider__item slider__item-02 mt0">
           <Container>
               <div className="slider__content ">
-                  <h4 className="text-light mb-3">For Rent $70 Per Day</h4>
-                  <h1 className="text-light mb-4">Reserve Now and Get 50% Off</h1>
+                  <h4 className="text-light mb-3">For Rent $90 Per Day</h4>
+                  <h1 className="text-light mb-4">Reserve Now and Get 60% Off</h1>
 
                   <button className="btn reserve__btn mt-4">
                       <Link to="/cars">Reserve Now</Link>
@@ -44,8 +44,8 @@ const HeroSlider = () => {
       <div className="slider__item slider__item-03 mt0">
           <Container>
               <div className="slider__content ">
-                  <h4 className="text-light mb-3">For Rent $70 Per Day</h4>
-                  <h1 className="text-light mb-4">Reserve Now and Get 50% Off</h1>
+                  <h4 className="text-light mb-3">For Rent $50 Per Day</h4>
+                  <h1 className="text-light mb-4">Reserve Now and Get 30% Off</h1>
 
                   <button className="btn reserve__btn mt-4">
                       <Link to="/cars">Reserve Now</Link>
