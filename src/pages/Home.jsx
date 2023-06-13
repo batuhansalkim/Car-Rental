e49d518bdd145dadf1f97d2fd8a417de8a560428
batuhan <batusalkim11@gmail.com>
2023-06-14 00:18:06 +0300

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import HeroSlider from '../components/UI/HeroSlider'
+import Helmet from '../components/Helmet/Helmet'
+import "../styles/hero-slider.css";
 
 function Home() {
-  return (
-    <div>Home</div>
-  )
+  return <Helmet title="Home">
+    <section className='p-0 hero__slider-section'>
+      <HeroSlider/>
+    </section>
+  </Helmet>
 }
 
 export default Home
