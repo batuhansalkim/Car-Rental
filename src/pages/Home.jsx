42030a0d981d19f2f1141398242289d2a1c1f1cc
batuhan <batusalkim11@gmail.com>
2023-06-21 17:13:05 +0300

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,6 +9,7 @@ import ServicesList from '../components/UI/ServicesList';
 import CarItem from '../components/UI/CarItem';
 import carData from "../assets/data/carData";
 import BecomeDriverSection from '../components/UI/BecomeDriverSection';
+import Testimonial from '../components/UI/Testimonial';
 
 function Home() {
   return <Helmet title="Home">
@@ -63,6 +64,19 @@ function Home() {
       </Container>
     </section>
     <BecomeDriverSection/>
+
+    <section>
+        <Container>
+          <Row>
+            <Col lg="12" className="mb-4 text-center">
+              <h6 className="section__subtitle">Our clients says</h6>
+              <h2 className="section__title">Testimonials</h2>
+            </Col>
+
+            <Testimonial />
+          </Row>
+        </Container>
+      </section>
   </Helmet>
 }
 
