a90ad253d5c8ba5260009882b878d48f295d063d
batuhan <batusalkim11@gmail.com>
2023-06-21 15:44:24 +0300

    update

--- src/components/UI/CarItem.js
+++ src/components/UI/CarItem.js
@@ -1,20 +1,21 @@
 import React from 'react'
 import {Col} from "reactstrap";
 import {Link} from "react-router-dom";
+import "../../styles/car-item.css";
 
 function CarItem({item}) {
     // const { imgUrl, model, carName, automatic, speed, price } = props.item;
   return <Col lg="4" md="4" sm="6" className='mb-5' >
   <div className='car__item'> 
     <div className="car__img">
-              <img src={item.imgUrl} alt="" className="w-100" />
+            <img src={item.imgUrl} alt="" className="w-100" />
     </div>
 
     <div className="car__item-content mt-4">
         <h4 className="section__title text-center">{item.carName}</h4>
         <h6 className="rent__price text-center mt-">${item.price}.00 <span>/ Day</span></h6>
 
-              <div className="car__item-info d-flex align-items-center justify-content-between mt-3 mb-4">
+            <div className="car__item-info d-flex align-items-center justify-content-between mt-3 mb-4">
                   <span className=" d-flex align-items-center gap-1">
                       <i class="ri-car-line"></i> {item.model}
                   </span>
@@ -24,13 +25,15 @@ function CarItem({item}) {
                   <span className=" d-flex align-items-center gap-1">
                       <i class="ri-timer-flash-line"></i> {item.speed}
                   </span>
-                </div>
-                <button className=" w-50 car__item-btn car__btn-rent">
-                  <Link to={`/cars/${item.carName}`}>Rent</Link>
-                </button>
-              <button className=" w-50 car__item-btn car__btn-details">
-                  <Link to={`/cars/${item.carName}`}>Details</Link>
-              </button>
+            </div>
+
+            <button className="w-50 car__item-btn car__btn-rent">
+                <Link to={`/cars/${item.carName}`}>Rent</Link>
+            </button>
+
+            <button className="w-50 car__item-btn car__btn-details">
+                <Link to={`/cars/${item.carName}`}>Details</Link>
+            </button>
               {/* span>
               </div>
 
