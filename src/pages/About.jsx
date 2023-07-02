b98eb7f71e9adc82bf90745ef09399a77728262c
batuhan <batusalkim11@gmail.com>
2023-07-03 00:01:20 +0300

    update

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,14 @@
 import React from "react"
-
+import Helmet from "../components/Helmet/Helmet";
+import CommonSection from "../components/UI/CommonSection";
+import AboutSection from "../components/UI/AboutSection";
 const About = ()=>{
     return(
-        <div>
-            About
-        </div>
+        <Helmet title="About">
+            <CommonSection title="About Us"/>
+            <AboutSection/>
+            
+        </Helmet>
     )
 }
 
